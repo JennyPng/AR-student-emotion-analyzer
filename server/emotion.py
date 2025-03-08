b0c7e8fe922080ca4eb7120f6ab2894f4062f4f9
Jenny Peng <jennypscale@gmail.com>
2025-03-07 23:38:53 -0800

    emotion recognition

--- server/emotion.py
+++ server/emotion.py
@@ -1,9 +1,33 @@
 import cv2
 import numpy as np
 from ultralytics import YOLO
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
 
-# load pretrained yolo11 model
-model = YOLO("yolo11n.pt")
+# load pretrained models
+face_model = YOLO("yolov11n-face.pt", verbose=False)
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+emotion_model.load_weights("model.h5")
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 # webcam, 0 for default
 cap = cv2.VideoCapture(0) 
@@ -12,6 +36,29 @@ while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    # Detect faces
+    results = face_model(frame)
+
+    for r in results:
+        for box in r.boxes:
+            # face bounding box
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+            # crop face, convert to grayscale, resize, normalize, reshape
+            face = frame[y1:y2, x1:x2]
+            face_gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            face_resized = cv2.resize(face_gray, (48, 48))
+            face_normalized = face_resized / 255.0 
+            face_reshaped = np.expand_dims(np.expand_dims(face_normalized, -1), 0)
+
+            # prediction
+            pred = emotion_model.predict(face_reshaped, verbose=0)
+            max_val = int(np.argmax(pred))
+            emotion = emotion_dict[max_val]
+
+            cv2.putText(frame, emotion, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     # display webcam
     cv2.imshow("Emotion Analyzer", frame)
