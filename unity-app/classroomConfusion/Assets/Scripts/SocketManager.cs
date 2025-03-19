e1d4016e98d0fd9e46b9475c83ef2f5accd30cde
Jenny Peng <jennypscale@gmail.com>
2025-03-18 21:25:53 -0700

    grokified unity

--- unity-app/classroomConfusion/Assets/Scripts/SocketManager.cs
+++ unity-app/classroomConfusion/Assets/Scripts/SocketManager.cs
@@ -1,124 +1,187 @@
-// Unity Client (Unity C# Script - Attach to a GameObject)
-using System;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
+using System.Collections;
 using UnityEngine;
-using System.Collections.Generic;
+using UnityEngine.Networking;
 using Newtonsoft.Json;
-using System.Collections; // Install via Unity Package Manager: Newtonsoft Json
 
-public class SocketClient : MonoBehaviour
+public class ApiClient : MonoBehaviour
 {
-
-    //public string serverIP = "10.18.156.162";
-    //public string serverIP = "205.175.106.52"; // 
-    public string serverIP = "172.20.10.3"; // MINE ON HOTSPOT
-
-    //public string serverIP = "10.19.108.229"; // brians
-    //public string serverIP = "172.20.10.2"; // brians on my hotspot
-
-    public int serverPort = 65432;
-
-    private TcpClient client;
-    private NetworkStream stream;
-
-    private bool isRunning;
-
+    private string serverUrl = "https://bluejay-novel-ghoul.ngrok-free.app/data"; // Ngrok URL
     private TextManager textManager;
 
+    // Start is called before the first frame update
     void Start()
     {
         textManager = TextManager.Instance;
-        ConnectToServer();
+        StartCoroutine(GetData());
     }
 
-    void ConnectToServer()
+    IEnumerator GetData()
     {
-        try
+        using (UnityWebRequest request = UnityWebRequest.Get(serverUrl))
         {
-            client = new TcpClient(AddressFamily.InterNetwork);
-            client.Connect(serverIP, serverPort);
+            yield return request.SendWebRequest();
 
-            stream = client.GetStream();
-            isRunning = true;
-
-            StartCoroutine(ReceiveData());
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("Socket error: " + e);
-            Invoke(nameof(ConnectToServer), 3f);
-        }
-    }
-
-    IEnumerator ReceiveData()
-    {
-        byte[] receiveBuffer = new byte[1024];
-        StringBuilder stringBuilder = new StringBuilder(); // Buffer for incomplete JSON
-
-        while (isRunning)
-        {
-            try
+            if (request.result == UnityWebRequest.Result.ConnectionError ||
+                request.result == UnityWebRequest.Result.ProtocolError)
             {
-                int bytesRead = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
-                if (bytesRead > 0)
+                Debug.LogError("Error: " + request.error);
+            }
+            else
+            {
+                try
                 {
-                    string receivedData = Encoding.UTF8.GetString(receiveBuffer, 0, bytesRead);
-                    stringBuilder.Append(receivedData);
+                    string jsonString = request.downloadHandler.text;
+                    ConfusionData confusionData = JsonConvert.DeserializeObject<ConfusionData>(jsonString);
 
-                    string data = stringBuilder.ToString();
-
-                    if (data.Contains("\n")) // Check for end of JSON
+                    Debug.Log($"Confusion Level: {confusionData.confusion}");
+                    foreach (string topic in confusionData.confusing_topics)
                     {
-                        string[] jsonStrings = data.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-                        foreach (string jsonString in jsonStrings)
-                        {
-
-                            try
-                            {
-                                ConfusionData confusionData = JsonConvert.DeserializeObject<ConfusionData>(jsonString);
-
-                                Debug.Log("received: " + confusionData.confusion.ToString());
-
-                                if (confusionData != null)
-                                {
-                                    textManager.updateConfusionData(confusionData);
-                                }
-
-                                stringBuilder.Clear(); // Clear the buffer
-                            }
-                            catch (JsonException e)
-                            {
-                                Debug.LogError("JSON parsing error: " + e);
-                                // Handle the error, potentially by waiting for more data
-                            }
-                        }
+                        Debug.Log($"Confusing Topic: {topic}");
                     }
 
+                    // Update the UI through the TextManager
+                    if (confusionData != null)
+                    {
+                        textManager.updateConfusionData(confusionData);
+                    }
                 }
-            }
-            catch (SocketException socketException)
-            {
-                Debug.LogError("Socket exception: " + socketException);
-                break;
-            }
-            catch (System.IO.IOException ioException)
-            {
-                Debug.LogError("IO exception: " + ioException);
-                break;
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Receive error: " + e);
-                break;
-            }
+                catch (JsonException e)
+                {
+                    Debug.LogError($"JSON parsing error: {e.Message}");
+                }
+
+            //Debug.Log("Response: " + request.downloadHandler.text);
+            //// Deserialize JSON response
+            //DataResponse response = JsonConvert.DeserializeObject<DataResponse>(request.downloadHandler.text);
+            //Debug.Log("Message: " + response.message);
+            //Debug.Log("Value: " + response.value);
         }
-        yield return null;
-    }
-
-    void Update()
-    {
-
+        }
     }
 }
+
+
+//// Unity Client (Unity C# Script - Attach to a GameObject)
+//using System;
+//using System.Net.Sockets;
+//using System.Text;
+//using System.Threading;
+//using UnityEngine;
+//using System.Collections.Generic;
+//using Newtonsoft.Json;
+//using System.Collections; // Install via Unity Package Manager: Newtonsoft Json
+
+//public class SocketClient : MonoBehaviour
+//{
+
+//    //public string serverIP = "10.18.156.162";
+//    //public string serverIP = "205.175.106.52"; // 
+//    public string serverIP = "172.20.10.3"; // MINE ON HOTSPOT
+
+//    //public string serverIP = "10.19.108.229"; // brians
+//    //public string serverIP = "172.20.10.2"; // brians on my hotspot
+
+//    public int serverPort = 65432;
+
+//    private TcpClient client;
+//    private NetworkStream stream;
+
+//    private bool isRunning;
+
+//    private TextManager textManager;
+
+//    void Start()
+//    {
+//        textManager = TextManager.Instance;
+//        ConnectToServer();
+//    }
+
+//    void ConnectToServer()
+//    {
+//        try
+//        {
+//            client = new TcpClient(AddressFamily.InterNetwork);
+//            client.Connect(serverIP, serverPort);
+
+//            stream = client.GetStream();
+//            isRunning = true;
+
+//            StartCoroutine(ReceiveData());
+//        }
+//        catch (Exception e)
+//        {
+//            Debug.LogError("Socket error: " + e);
+//            Invoke(nameof(ConnectToServer), 3f);
+//        }
+//    }
+
+//    IEnumerator ReceiveData()
+//    {
+//        byte[] receiveBuffer = new byte[1024];
+//        StringBuilder stringBuilder = new StringBuilder(); // Buffer for incomplete JSON
+
+//        while (isRunning)
+//        {
+//            try
+//            {
+//                int bytesRead = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+//                if (bytesRead > 0)
+//                {
+//                    string receivedData = Encoding.UTF8.GetString(receiveBuffer, 0, bytesRead);
+//                    stringBuilder.Append(receivedData);
+
+//                    string data = stringBuilder.ToString();
+
+//                    if (data.Contains("\n")) // Check for end of JSON
+//                    {
+//                        string[] jsonStrings = data.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+//                        foreach (string jsonString in jsonStrings)
+//                        {
+
+//                            try
+//                            {
+//                                ConfusionData confusionData = JsonConvert.DeserializeObject<ConfusionData>(jsonString);
+
+//                                Debug.Log("received: " + confusionData.confusion.ToString());
+
+//                                if (confusionData != null)
+//                                {
+//                                    textManager.updateConfusionData(confusionData);
+//                                }
+
+//                                stringBuilder.Clear(); // Clear the buffer
+//                            }
+//                            catch (JsonException e)
+//                            {
+//                                Debug.LogError("JSON parsing error: " + e);
+//                                // Handle the error, potentially by waiting for more data
+//                            }
+//                        }
+//                    }
+
+//                }
+//            }
+//            catch (SocketException socketException)
+//            {
+//                Debug.LogError("Socket exception: " + socketException);
+//                break;
+//            }
+//            catch (System.IO.IOException ioException)
+//            {
+//                Debug.LogError("IO exception: " + ioException);
+//                break;
+//            }
+//            catch (Exception e)
+//            {
+//                Debug.LogError("Receive error: " + e);
+//                break;
+//            }
+//        }
+//        yield return null;
+//    }
+
+//    void Update()
+//    {
+
+//    }
+//}
